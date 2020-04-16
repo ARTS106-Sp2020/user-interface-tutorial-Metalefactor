5dc64516908b75998b234c5c23622ee450313de9
Matt Perrotti <60189116+Metalefactor@users.noreply.github.com>
2020-04-16 00:30:33 -0400

    Part 3 - Update Scene
    about to add restart dialouge

--- Assets/RocketMouse/Scripts/MouseController.cs
+++ Assets/RocketMouse/Scripts/MouseController.cs
@@ -1,24 +1,24 @@
 ﻿/*
  * Copyright (c) 2018 Razeware LLC
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
  * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
- *    
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,8 +31,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
-public class MouseController : MonoBehaviour 
+public class MouseController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
@@ -49,21 +50,22 @@ public class MouseController : MonoBehaviour
     private bool grounded;
     private bool dead = false;
     private uint coins = 0;
+    public Text coinsLabel;
 
-    void Start () 
+    void Start ()
     {
-        animator = GetComponent<Animator>();	
+        animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate () 
+    void FixedUpdate ()
     {
         bool jetpackActive = Input.GetButton("Fire1");
 	    jetpackActive = jetpackActive && !dead;
-	    if (jetpackActive) 
-	    { 
+	    if (jetpackActive)
+	    {
 	        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
 	    }
-	    if (!dead) 
+	    if (!dead)
 	    {
 	        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
 	        newVelocity.x = forwardMovementSpeed;
@@ -73,36 +75,36 @@ public class MouseController : MonoBehaviour
 	    AdjustJetpack(jetpackActive);
 	    AdjustFootstepsAndJetpackSound(jetpackActive);
 	    parallax.offset = transform.position.x;
-    } 
+    }
 
-    void UpdateGroundedStatus() 
+    void UpdateGroundedStatus()
     {
         grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
         animator.SetBool("grounded", grounded);
     }
 
-    void AdjustJetpack (bool jetpackActive) 
+    void AdjustJetpack (bool jetpackActive)
     {
   	    ParticleSystem.EmissionModule jpEmission = jetpack.emission;
 	    jpEmission.enabled = !grounded;
-	    jpEmission.rateOverTime = jetpackActive ? 300.0f : 75.0f; 
+	    jpEmission.rateOverTime = jetpackActive ? 300.0f : 75.0f;
     }
 
-    void OnTriggerEnter2D(Collider2D collider) 
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.CompareTag("Coins")) 
+        if (collider.gameObject.CompareTag("Coins"))
         {
 	        CollectCoin(collider);
-        } 
-        else 
+        }
+        else
         {
             HitByLaser(collider);
-	    } 
+	    }
     }
 
-    void HitByLaser(Collider2D laserCollider) 
+    void HitByLaser(Collider2D laserCollider)
     {
-        if (!dead) 
+        if (!dead)
         {
             laserCollider.gameObject.GetComponent<AudioSource>().Play();
 	    }
@@ -110,50 +112,18 @@ public class MouseController : MonoBehaviour
 	    animator.SetBool("dead", true);
     }
 
-    void CollectCoin(Collider2D coinCollider) 
+    void CollectCoin(Collider2D coinCollider)
     {
         coins++;
         Destroy(coinCollider.gameObject);
         AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
+        coinsLabel.text = coins.ToString();
     }
 
-    void OnGUI() 
-    {
-        DisplayCoinsCount();
-        DisplayRestartButton();
-    }
-
-    void DisplayCoinsCount() 
-    {
-	    Rect coinIconRect = new Rect(10, 10, 32, 32);
-	    GUI.DrawTexture(coinIconRect, coinIconTexture);                         
-		
-	    GUIStyle style = new GUIStyle();
-	    style.fontSize = 30;
-	    style.fontStyle = FontStyle.Bold;
-	    style.normal.textColor = Color.yellow;
-
-	    Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
-	    GUI.Label(labelRect, coins.ToString(), style);
-    }
-
-    void DisplayRestartButton() 
-    {
-        if (dead && grounded) 
-        {
-            Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
-	        if (GUI.Button(buttonRect, "Tap to restart!")) 
-	        {
-	        	
-				SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
-	        }
-	    }
-    }
-
-  void AdjustFootstepsAndJetpackSound(bool jetpackActive) 
+  void AdjustFootstepsAndJetpackSound(bool jetpackActive)
   {
       footstepsAudio.enabled = !dead && grounded;
       jetpackAudio.enabled =  !dead && !grounded;
-	  jetpackAudio.volume = jetpackActive ? 1.0f : 0.5f;        
+	  jetpackAudio.volume = jetpackActive ? 1.0f : 0.5f;
   }
 }
